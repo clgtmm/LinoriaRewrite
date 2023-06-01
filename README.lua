1252d5eaa88a30da4f33059a2fd1637ec5aea197
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-06-01 11:02:40 +0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -3607,7 +3607,7 @@ function Library:CreateWindow(...)
             if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
             end
-        elseif Input.KeyCode == Enum.KeyCode.RightControl or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
+        elseif Input.KeyCode == Enum.KeyCode[_G.UiBind] or (Input.KeyCode == Enum.KeyCode[_G.UiBind] and (not Processed)) then
             task.spawn(Library.Toggle)
         end
     end))
